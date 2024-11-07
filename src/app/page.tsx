ba77b3f5b7db60de4aec5065122b86932c288a8e
Joseph Akinlade <akinaldeirede@gmail.com>
2024-11-07 09:56:00 +0100

    deleted template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,8 @@ import Hero from "@/components/hero"
 import Work from "@/components/work"
 import React from "react"
 
-export default function Home() {
+export default function HomePage() {
   return <>
-  
     <Hero />
     <Work />
   </>
