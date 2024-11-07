657e1f0053a144e3e218be807b392a998d4a8a70
Joseph Akinlade <akinaldeirede@gmail.com>
2024-11-07 09:40:36 +0100

    added font/index.ts

--- src/app/contact/page.tsx
+++
@@ -1,5 +0,0 @@
-"use client"
-import React from "react"
-export default function ContactPage() {
-  return <h1 className="font-bold text-9xl text-center page-title translate-y-[100px]">Contact</h1>
-}
